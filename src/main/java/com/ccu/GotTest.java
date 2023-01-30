150afe7eeaba1777fcb25b474a2244c52c12cf26
tuwenjie <1562431918@qq.com>
2023-01-30 14:18:22 +0800

    第一次提交项目

---
+++ src/main/java/com/ccu/GotTest.java
@@ -0,0 +1,11 @@
+package com.ccu;
+
+/**
+ * @author twj
+ * @date 2023/1/30-14:14
+ **/
+public class GotTest {
+    public static void main(String[] args) {
+        System.out.println("hello Git");
+    }
+}
